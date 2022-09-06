86e10736c0178a77eb27d800321538c017ae8909
jinjun <757258777@qq.com>
2022-09-06 11:03:58 +0800

    [fix]:美团加入token 验证传递

--- src/MeituanDelivery/MeituanDelivery.php
+++ src/MeituanDelivery/MeituanDelivery.php
@@ -4,9 +4,9 @@ namespace Cblink\Service\ShopDelivery\MeituanDelivery;
 
 
 use Cblink\Service\Foundation\Container;
-
+use Cblink\Service\ShopDelivery\Kernel;
 /**
- *
+ * @property-read \Cblink\Service\Foundation\AccessToken $access_token
  *
  * @property-read Shop\Client $shop       门店
  * @property-read Order\Client $order     订单
@@ -14,6 +14,7 @@ use Cblink\Service\Foundation\Container;
 class MeituanDelivery extends Container
 {
     protected array $providers = [
+        Kernel\ServiceProvider::class,
         Order\ServiceProvider::class,
         Shop\ServiceProvider::class,
     ];
